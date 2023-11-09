f52a41651eedf76d0f92449006a86d602f953707
Pedro Henrique Gomes Acre <133990444+PHTML266@users.noreply.github.com>
2023-11-09 10:25:33 -0300

    Delete demo directory

--- demo/src/main/java/com/biblioteca/demo/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.biblioteca.demo.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
-}
